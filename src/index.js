d178889bf4cb1c3c37eae33ae3b76b772af073d0
Michael Halaj <michaelhalaj7@gmail.com>
2023-05-19 11:42:54 -0400

    add task object

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles.css';
+
+const addTask = document.querySelector('.add-task');
+console.log(addTask);
+addTask.addEventListener('click', () => {
+  console.log('add task');
+});
---
+++ src/task.js
@@ -0,0 +1,5 @@
+const task = (title, description, dueDate, priority) => ({
+  title, description, dueDate, priority,
+});
+
+export default task;
