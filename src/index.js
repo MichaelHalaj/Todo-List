6fb5d5443ff12a96acaf61ea0e1237540906d9f6
Michael Halaj <michaelhalaj7@gmail.com>
2023-05-19 13:17:01 -0400

    Add new project form

--- src/index.js
+++ src/index.js
@@ -10,7 +10,30 @@ const myProjectsTitle = document.querySelector('#projects-title');
 
 const todoListNode = document.querySelector('.todo-list');
 
+const addNewProjectNode = document.querySelector('#add-project');
+
+const sideBarListNode = document.querySelector('.sidebar-list');
+
+const projectFormNode = document.querySelector('#project-form');
+
 const projectsList = [];
+
+addNewProjectNode.addEventListener('click', () => {
+  projectFormNode.classList.remove('hide');
+  addNewProjectNode.classList.add('hide');
+});
+/* const projectFormNode = document.createElement('form');
+const projectFormInputNode = document.createElement('input');
+projectFormInputNode.setAttribute('type', 'text');
+projectFormInputNode.setAttribute('maxlength', '20');
+projectFormNode.appendChild(projectFormInputNode);
+const projectFormSubmitNode = document.createElement('input');
+projectFormSubmitNode.setAttribute('type', 'submit');
+projectFormSubmitNode.setAttribute('value', 'Add');
+projectFormNode.appendChild(projectFormSubmitNode);
+addNewProjectNode.addEventListener('click', () => {
+  sideBarListNode.insertBefore(projectFormNode, addNewProjectNode);
+}); */
 
 /**
  * Set up the page with a default today project
