17d012140ca59bc4ae70baabc61a43acf0c5547f
Michael Halaj <michaelhalaj7@gmail.com>
2023-05-20 12:38:34 -0400

    Fix cancel button to not submit

--- src/index.js
+++ src/index.js
@@ -12,40 +12,62 @@ const todoListNode = document.querySelector('.todo-list');
 
 const addNewProjectNode = document.querySelector('#add-project');
 
+const cancelNewProjectNode = document.querySelector('#project-cancel');
+
 const sideBarListNode = document.querySelector('.sidebar-list');
 
 const projectFormNode = document.querySelector('#project-form');
 
+const addTask = document.querySelector('.add-task');
+
 const projectsList = [];
 
-addNewProjectNode.addEventListener('click', () => {
+function hideProjectForm() {
+  projectFormNode.reset();
+  projectFormNode.classList.add('hide');
+  projectFormNode.classList.remove('col');
+  addNewProjectNode.classList.remove('hide');
+}
+
+function showProjectForm() {
   projectFormNode.classList.remove('hide');
   projectFormNode.classList.add('col');
   addNewProjectNode.classList.add('hide');
+}
+
+cancelNewProjectNode.addEventListener('click', () => {
+  hideProjectForm();
+});
+
+addNewProjectNode.addEventListener('click', () => {
+  showProjectForm();
 });
 
 projectFormNode.addEventListener('submit', (e) => {
   e.preventDefault();
   console.log(e.target[0].value);
-  const newProject = new Project(e.target[0].value);
-  const newProjectNode = document.createElement('button');
-  newProjectNode.textContent = newProject.getProjectTitle;
-  newProjectNode.classList.add('project-item');
-  projectsList.push(newProject);
-  myProjectsTitle.appendChild(newProjectNode);
-
-  newProjectNode.addEventListener('click', () => {
-    removeAllChildren(todoListNode);
-    newProject.getTaskList.forEach((task) => {
-      const taskNode = document.createElement('div');
-      taskNode.innerText = task.title;
-      todoListNode.appendChild(taskNode);
+  console.log(e.submitter.id);
+  if (e.submitter.id === 'project-cancel') {
+    hideProjectForm();
+  } else {
+    const newProject = new Project(e.target[0].value);
+    const newProjectNode = document.createElement('button');
+    newProjectNode.textContent = newProject.getProjectTitle;
+    newProjectNode.classList.add('project-item');
+    projectsList.push(newProject);
+    myProjectsTitle.appendChild(newProjectNode);
+    newProjectNode.addEventListener('click', () => {
+      removeAllChildren(todoListNode);
+      newProject.getTaskList.forEach((task) => {
+        const taskNode = document.createElement('div');
+        taskNode.innerText = task.title;
+        todoListNode.appendChild(taskNode);
+      });
     });
-  });
-  projectFormNode.classList.add('hide');
-  projectFormNode.classList.remove('col');
-  addNewProjectNode.classList.remove('hide');
-  projectFormNode.reset();
+    hideProjectForm();
+  }
+
+  
 });
 
 /**
@@ -66,12 +88,6 @@ todayProjectNode.addEventListener('click', () => {
     todoListNode.appendChild(taskNode);
   });
 });
-
-
-const addTask = document.querySelector('.add-task');
-
-
-
 
 console.log(addTask);
 addTask.addEventListener('click', () => {
