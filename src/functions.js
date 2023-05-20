fc4995cdc7fe84b30014597233cc046e12593c13
Michael Halaj <michaelhalaj7@gmail.com>
2023-05-20 13:28:15 -0400

    Create addtask form and refactor code

--- src/functions.js
+++ src/functions.js
@@ -3,5 +3,35 @@ const removeAllChildren = (node) => {
     node.removeChild(node.firstChild);
   }
 };
+const createAddTaskNode = () => {
+  const node = document.createElement('button');
+  node.classList.add('add-task');
+  node.innerText = 'Add Task';
+  return node;
+};
+const createTaskForm = () => {
+  // Create form element
+  const form = document.createElement('form');
+  form.setAttribute('id', 'task-form');
 
-export default removeAllChildren;
+  // Create title of task element
+  const title = document.createElement('input');
+  title.setAttribute('id', 'title');
+  title.setAttribute('type', 'text');
+  title.setAttribute('maxlength', '30');
+
+  // Create description input
+  const description = document.createElement('textarea');
+  description.setAttribute('id', 'description');
+
+  // Create date input
+  const date = document.createElement('input');
+  date.setAttribute('type', 'datetime-local');
+  date.setAttribute('id', 'due-date');
+
+  form.appendChild(title);
+  form.appendChild(description);
+  form.appendChild(date);
+  return form;
+};
+export { removeAllChildren, createAddTaskNode, createTaskForm };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 import Project from './project';
 import Task from './task';
-import removeAllChildren from './functions';
+import { removeAllChildren, createAddTaskNode, createTaskForm } from './functions';
 
 /**
  * Attach all project titles under the My Projects title within the DOM
@@ -43,53 +43,40 @@ addNewProjectNode.addEventListener('click', () => {
   showProjectForm();
 });
 
+function createNewProject(projectTitle) {
+  const newProject = new Project(projectTitle);
+  const newProjectNode = document.createElement('button');
+  newProjectNode.textContent = newProject.getProjectTitle;
+  newProjectNode.classList.add('project-item');
+  projectsList.push(newProject);
+  myProjectsTitle.appendChild(newProjectNode);
+  newProjectNode.addEventListener('click', () => {
+    removeAllChildren(todoListNode);
+    newProject.getTaskList.forEach((task) => {
+      const taskNode = document.createElement('div');
+      taskNode.innerText = task.title;
+      todoListNode.appendChild(taskNode);
+    });
+    const addTaskNode = createAddTaskNode();
+    todoListNode.appendChild(addTaskNode);
+    addTaskNode.addEventListener('click', () => {
+      console.log('task node');
+      todoListNode.insertBefore(createTaskForm(), addTaskNode);
+    });
+  });
+  hideProjectForm();
+}
+
 projectFormNode.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(e.target[0].value);
-  console.log(e.submitter.id);
   if (e.submitter.id === 'project-cancel') {
     hideProjectForm();
   } else {
-    const newProject = new Project(e.target[0].value);
-    const newProjectNode = document.createElement('button');
-    newProjectNode.textContent = newProject.getProjectTitle;
-    newProjectNode.classList.add('project-item');
-    projectsList.push(newProject);
-    myProjectsTitle.appendChild(newProjectNode);
-    newProjectNode.addEventListener('click', () => {
-      removeAllChildren(todoListNode);
-      newProject.getTaskList.forEach((task) => {
-        const taskNode = document.createElement('div');
-        taskNode.innerText = task.title;
-        todoListNode.appendChild(taskNode);
-      });
-    });
-    hideProjectForm();
+    createNewProject(e.target[0].value);
   }
-
-  
 });
 
 /**
  * Set up the page with a default today project
  */
-const todayProject = new Project('Today');
-const todayProjectNode = document.createElement('button');
-todayProjectNode.textContent = todayProject.getProjectTitle;
-todayProjectNode.classList.add('project-item');
-projectsList.push(todayProject);
-myProjectsTitle.appendChild(todayProjectNode);
-
-todayProjectNode.addEventListener('click', () => {
-  removeAllChildren(todoListNode);
-  todayProject.getTaskList.forEach((task) => {
-    const taskNode = document.createElement('div');
-    taskNode.innerText = task.title;
-    todoListNode.appendChild(taskNode);
-  });
-});
-
-console.log(addTask);
-addTask.addEventListener('click', () => {
-  console.log('add task');
-});
+createNewProject('Today');
